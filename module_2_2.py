383ae48839cd2a3f84dfcec2ebf6635d4b65f59e
DiVaXa <diana-shine@mail.ru>
2024-09-20 20:18:42 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,9 @@
+first =10
+second =15
+third =20
+if first == second or first == third or second == third:
+    print(2)
+elif first == second and first== third and second== third:
+    print(3)
+elif first!= second and first!=third and second!=third:
+    print(0)
