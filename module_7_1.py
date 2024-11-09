fdf5956addfa87905562fdd503a812bb59b35ca9
DiVaXa <diana-shine@mail.ru>
2024-11-09 21:54:12 +0300

    Update module_7_1.py

--- module_7_1.py
+++ module_7_1.py
@@ -5,38 +5,37 @@ class Product:
         self.name = name
         self.weight = weight
         self.category = category
-
     def __str__(self):
-        return f'{self.name}, {self.weight}, {self.category}.'
+        return(f'{self.name}, {self.weight}, {self.category}')
 
-class Shop:
-
-    __file_name = 'products.txt'
+class Shop(Product):
+    __file_name='products.txt'
 
     def get_products(self):
         file = open(self.__file_name, 'r+')
         st = file.read()
         file.close()
-        return f'{st}'
-
+        print(f'{st}')
     def add(self, *products):
-        file_get = self.get_products()
         for i in products:
-            if self.get_products().find(f'{i.name},') == -1:
-                file = open(self.__file_name, 'a')
-                file.write(f'{i}\n')
-                file.close()
+            s = (str(i))
+            file = open(self.__file_name, 'r+')
+            f = file.read()
+            file.close()
+            if s in f:
+                print(f'Продукт {s} уже есть в магазине')
             else:
-                print(f'Продукт {i.name} уже есть в магазине.')
+                file = open(self.__file_name, 'a')
+                file.write(f'{s}\n')
+                file.close()
 
-
-s1 = Shop()
+s1 = Shop('',0 , '')
 p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
-
-print(p2) # __str__
-
+#
+print(p2)  # __str__
+s1.get_products()
 s1.add(p1, p2, p3)
 
 print(s1.get_products())
